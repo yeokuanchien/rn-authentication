3f40e804cd298344805ff69fdc85d26261b3ab22
Yeo Kuan Chien <kuan.chien.yeo@accenture.com>
2020-08-28 12:24:25 +0800

    Created Button.js

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Button = ({ params }) => (
+  <View>
+    <Text>Button</Text>
+  </View>
+);
+
+export default Button;
